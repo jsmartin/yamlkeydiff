b3d7d54e435e7106207434c477886028658a4282
James Martin <jmartin@ansible.com>
2014-09-18 12:10:52 +0200

    Using unified diff for output format.

--- vardiff.py
+++ vardiff.py
@@ -2,6 +2,7 @@
 
 import yaml
 import argparse
+from difflib import unified_diff
 
 # new_vars_file is considered the authoritative source.
 # if new_vars_file has keys that are not present in old_vars_file, 
@@ -17,17 +18,10 @@ args = parser.parse_args()
 
 file1_data = yaml.load(open(args.new_vars_file).read())
 file2_data = yaml.load(open(args.old_vars_file).read())
+s1 = file1_data.keys()
+s2 = file2_data.keys()
+s1.sort()
+s2.sort()
 
-new_keys = list(set(file1_data.keys()).difference(file2_data.keys()))
-
-print "\nNew Keys:"
-print '\n'.join(new_keys)
-
-print "\nDepricated Keys:"
-depricated_keys = []
-
-for key in file2_data.keys():
-	if not key in file1_data.keys():
-		print key
-		depricated_keys.append(key)
-
+for key in unified_diff(s1, s2, fromfile=args.old_vars_file, tofile=args.new_vars_file):
+    print key
